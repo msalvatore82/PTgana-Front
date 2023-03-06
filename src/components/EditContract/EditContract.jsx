80e58a6ff714b0819f88985a3baffd2fdad24dd6
Matias Salvatore <msalvatore@ganaenergia.com>
2023-03-06 10:50:49 +0100

    actulizamos editcontract

--- src/components/EditContract/EditContract.jsx
+++ src/components/EditContract/EditContract.jsx
@@ -26,14 +26,14 @@ const EditContract = () => {
 
     useEffect(() => {
             setFormValues({
-                nombre: contract?.contract.nombre || "",
-                apellido1: contract?.contract.apellido1 || "",
-                apellido2: contract?.contract.apellido2 || "",
-                documento: contract?.contract.documento || "",
-                cp: contract?.contract.cp || "",
-                localidad: contract?.contract.localidad || "",
-                direccion: contract?.contract.direccion || "",
-                telefono: contract?.contract.telefono || "",
+                nombre: contract?.contract?.nombre || "",
+                apellido1: contract?.contract?.apellido1 || "",
+                apellido2: contract?.contract?.apellido2 || "",
+                documento: contract?.contract?.documento || "",
+                cp: contract?.contract?.cp || "",
+                localidad: contract?.contract?.localidad || "",
+                direccion: contract?.contract?.direccion || "",
+                telefono: contract?.contract?.telefono || "",
             });
     }, [contract]);
 
@@ -70,7 +70,7 @@ const EditContract = () => {
                 <input Value={contract?.contract?.apellido2 || ""} onChange={(e) => setFormValues({ ...formValues, apellido2: e.target.value })} name="apellido2"/>
                 <input Value={contract?.contract?.documento || ""} onChange={(e) => setFormValues({ ...formValues, documento: e.target.value })} name="documento"/>
                 <input Value={contract?.contract?.cp || ""} onChange={(e) => setFormValues({ ...formValues, cp: e.target.value })} name="cp"/>
-                <input Value={contract?.contract?.localidad.municipio_nombre || ""} onChange={(e) => setFormValues({ ...formValues, localidad: e.target.value })} name="localidad"/>
+                <input Value={contract?.contract?.localidad?.municipio_nombre || ""} onChange={(e) => setFormValues({ ...formValues, localidad: e.target.value })} name="localidad"/>
                 <input Value={contract?.contract?.direccion || ""} onChange={(e) => setFormValues({ ...formValues, direccion: e.target.value })} name="direccion"/>
                 <input Value={contract?.contract?.telefono || ""} onChange={(e) => setFormValues({ ...formValues, telefono: e.target.value })} name="telefono"/>
                 <Button type="primary" block htmlType="submit" className="btn" onClick={handleSubmit} >
